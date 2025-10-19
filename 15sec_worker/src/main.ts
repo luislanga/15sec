de4dc6fb784049ae0e88d401ea643285b7147bb5
Luis Langa <luisjunior@tokenlab.com.br>
2025-10-18 21:18:59 -0300

    (feat) Creates Worker Project and Health Check route

---
+++ 15sec_worker/src/main.ts
@@ -0,0 +1,14 @@
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  const configService = app.get(ConfigService);
+  const port = configService.getOrThrow<number>('SERVER_PORT');
+
+  await app.listen(port);
+}
+bootstrap();
